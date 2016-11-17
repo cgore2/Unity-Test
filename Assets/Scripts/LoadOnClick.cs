7ea899c1a9812686db249f8a0e38457f8ca87807
cgore2 <cagore10@gmail.com>
2016-11-17 14:48:41 -0600

    Unity stuff

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+    public void LoadScene(int name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
